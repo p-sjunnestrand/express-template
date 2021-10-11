7b39c8b25981abcabdf5c01c252a7276498508d7
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-10-11 21:34:45 +0200

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
+const host = '0.0.0.0';
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log("Server running on port " + port);
 })
